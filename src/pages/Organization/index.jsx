ea446454f3cca5dac573fda8d1817c5d57631937
fethabo <73237365+fethabo@users.noreply.github.com>
2024-07-18 17:07:05 -0300

    Update index.jsx

--- src/pages/Organization/index.jsx
+++ src/pages/Organization/index.jsx
@@ -13,13 +13,6 @@ export default function Organization() {
     const {data: organization, isFetching: isFetchingOrganization, isError: isErrorOrganization}= useGetOrganization({organizationId:idOrganization, enabled:!!idOrganization})
     const {data: paths, isFetching: isFetchingPaths, isError: isErrorPaths}= useGetOrganizationPaths({organizationId:idOrganization, enabled:!!idOrganization})
    
-
-/* TODO:
-4	Divulgator
-5	Moderator
-3	Admin
-7	Tester 
-*/
     const {data: roles, isFetching: rolesOrganization, isError: isErrorRolesOrganization} = useGetUserRolesInOrganization({organizationId:idOrganization, userId:user, enabled: !!idOrganization&&!!user})
     const navigate = useNavigate();
     return ( 
@@ -29,13 +22,9 @@ export default function Organization() {
             <Typography variant="h5">Pagina de la organización {organization?.name}</Typography>
             <Typography variant="subtitle1">{organization?.description}</Typography>
             <Button onClick={()=> navigate(`./edit`, {relative: 'path'})}>Editar</Button>          {/* TODO: si el usuario es admin de la organizacion puede editar la organizacion */}
-            <Button onClick={()=> navigate(`./users`, {relative: 'path'})}>Permisos</Button>          {/* TODO: si el usuario es admin de la organizacion puede editar la organizacion */}
-
-  
-           
+            <Button onClick={()=> navigate(`./users`, {relative: 'path'})}>Permisos</Button>          {/* TODO: si el usuario es admin de la organizacion puede editar la organizacion */}           
             <Typography variant="h6" align="left">Caminos de la organización</Typography>
             <CarouselPaths paths={paths} isError={isErrorPaths} isLoading={isFetchingPaths||isFetchingOrganization} />
-
             <Typography variant="h6" align="left">Contenidos de la organización</Typography>
             <Button onClick={()=> navigate(`./contents`, {relative: 'path'})}>Ver contenidos</Button> 
             <Outlet />
