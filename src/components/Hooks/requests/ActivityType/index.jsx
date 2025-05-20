3da5397e22c3b00c50c46a8639976421aa4c65d3
fethabo <fborgna_52@hotmail.com>
2025-05-19 22:10:39 -0300

    feat: agregado nompre de actividad y de orga en vista previa de content

--- src/components/Hooks/requests/ActivityType/index.jsx
+++ src/components/Hooks/requests/ActivityType/index.jsx
@@ -15,3 +15,18 @@ export function useGetActivityTypes({ ...rest}){
       )
     )
 }
+
+/**
+ * 
+ * @param {Object} params
+ * @param {string|number} params.id - The ID of the activity type to fetch
+ * @param {*} rest - Additional options
+ * @returns 
+ */
+export function useGetActivityType({ activityTypeId, ...rest }) {
+  return useApiQuery({
+    queryKey: ['useGetActivityType', activityTypeId],
+    endpoint: `/activity-types/${activityTypeId}`,
+    options: { ...rest },
+  });
+}
--- src/pages/MapConfig/ActivityInfo.jsx
+++ src/pages/MapConfig/ActivityInfo.jsx
@@ -8,55 +8,110 @@ import { useState } from "react";
 import ErrorBoundary from "@components/ErrorBoundary/ErrorBoundary";
 import Activity from "@pages/Activity";
 import SkeletonContent from "./SkeletonContent";
+import { useGetActivityTypeVersion } from "@components/Hooks/requests/ActivityTypeVersion";
+import { useGetActivityType } from "@components/Hooks/requests/ActivityType";
+import PersonIcon from '@mui/icons-material/Person';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import CallSplitIcon  from '@mui/icons-material/CallSplit';
+import { useGetOrganization } from "@components/Hooks/requests/Organizations";
+import { Preview } from "@mui/icons-material";
+
+function ContentDetails({ content, onEdit }) {
+    const { data: activityTypeVersion, isFetching: isFetchingActivityTypeVersion, isError: isErrorActivityTypeVersion}= useGetActivityTypeVersion({activityTypeVersionId: content?.activityTypeVersionId, enabled: !!content})
+    const { data: activityType, isFetching: isFetchingActivityType, isError: isErrorActivityType } = useGetActivityType({ activityTypeId: activityTypeVersion?.activityTypeId, enabled: !!activityTypeVersion }) 
+    const { data: organization, isFetching: isFetchingOrganization, isError: isErrorOrganization } = useGetOrganization({ organizationId: content?.organization, enabled: !!content?.organization })
+
+    if (isFetchingActivityTypeVersion || isFetchingActivityType || isFetchingOrganization) {
+        return <SkeletonContent />;
+    }
+
+    if (isErrorActivityTypeVersion || isErrorActivityType || isErrorOrganization) {
+        return <Alert severity="error">Hubo un error al obtener el tipo de actividad u organización</Alert>;
+    }
+
+    const organizationName = organization?.name || content.organization;
+    const activityTypeName = activityType?.name || content.activityTypeVersionId;
+
+    return (
+        <Card sx={{ marginBottom: 2, width: "100%", textAlign: 'left', padding: 1 }}>
+            <CardContent>
+                <Box display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap" mb={1}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' , width: '100%'}}>
+                        <Typography variant="h6">{content.description}</Typography>
+                        <Tooltip title="Editar" arrow>
+                            <IconButton variant="contained" color="secondary" onClick={onEdit}>
+                                <EditIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Box>
+                <Box display="flex" alignItems="center" gap={1} mb={0.5}>
+                    <Typography variant="body2" color="text.secondary" display="flex" alignItems="center" gap={0.5}>
+                        <PersonIcon fontSize="small" sx={{ verticalAlign: 'middle' }} /> {content.username}
+                    </Typography>
+                    {organizationName && (
+                        <Box display="flex" alignItems="center" gap={0.5} ml={2}>
+                            <Tooltip title="Organización verificada">
+                                <VerifiedIcon color="primary" fontSize="small" />
+                            </Tooltip>
+                            <Typography variant="body2" color="primary" fontWeight={500}>
+                                {organizationName}
+                            </Typography>
+                        </Box>
+                    )}
+                </Box>
+                <Box display="flex" alignItems="center" gap={1} mb={0.5}>
+                    <Typography variant="body2" color="text.secondary" display="flex" alignItems="center" gap={0.5}>
+                        <CallSplitIcon fontSize="small" sx={{ verticalAlign: 'middle' }} /> {activityTypeName}
+                    </Typography>
+                </Box>
+                <Box display="flex" gap="1em" alignItems="center" mt={1}>
+                    <TagsDisplay entityId={content?.contentId} entityType='contents' />
+                </Box>
+            </CardContent>
+
+            <CardContent sx={{ padding: 2, border: '1px solid #e0e0e0', borderRadius: '1em' }}>
+                <Box display="flex" alignItems="center" gap={1} mb={1}>
+                    <Tooltip title="Vista previa">
+                        {/* Puedes cambiar el icono por otro si prefieres */}
+                        <Box display="flex" alignItems="center">
+                            <Preview color="action" />
+                        </Box>
+                    </Tooltip>
+                    <Typography variant="body2" color="white">Vista previa:</Typography>
+                </Box>
+                <ErrorBoundary>
+                    <Activity id={content.contentId} />
+                </ErrorBoundary>
+            </CardContent>
+        </Card>
+    );
+}
+ContentDetails.propTypes = {
+    content: PropTypes.object,
+    onEdit: PropTypes.func.isRequired,
+};
 
 function ActivityInfo({activity}) {
     const {data: content, isFetching: isFetchingContent, isError: isErrorContent} = useGetContent({contentId: activity?.content, enabled: !!activity?.content})
-
+   
     const [openEditContent,setOpenEditContent] = useState(false)
 
-
-    return (  
-    <Stack textAlign={"left"}>
-        <Typography variant="h6">Actividad del level: </Typography>
-        <Typography variant="body1">Descripción: {activity?.description}</Typography>
-        <Typography variant="body1">Contenido: </Typography>
-        <EditContentDialog open={openEditContent} handleClose={() => { setOpenEditContent(false) }} content={content} />
-                {isFetchingContent ? (
-                    <SkeletonContent />
-                ): isErrorContent? <Alert severity="error">Hubo un error al obtener el contenido</Alert>:
-         <Card sx={{ marginBottom: 2, width: "100%", textAlign:'left' }}>
-                <CardContent>
-                  <Box display={"flex"} justifyContent={"space-between"} flexWrap={"wrap"}>
-                    <Typography variant="h6">{content.description}</Typography>    <Tooltip title="Editar" arrow>
-                        <IconButton variant="contained" color="secondary" onClick={() => {setOpenEditContent(true)}}>
-                            <EditIcon />
-                        </IconButton>
-                    </Tooltip>  
-                  </Box>
-                  <Typography variant="body2">Usuario: {content.username}</Typography>
-                  <Typography variant="body2">Organización: {content.organization}</Typography>
-                  <Typography variant="body2">Tipo de Actividad: {content.activityTypeVersionId}</Typography>
-                  <Box display="flex" gap="1em" alignItems={"center"}>
-                    <Typography variant="caption">Tags: </Typography> 
-                    <TagsDisplay entityId={content?.contentId} entityType='contents' />
-                  </Box>
-                </CardContent>
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding: 2 }}>
-                  
-                  
-                </Box>
-                <CardContent>
-                    <Typography variant="body2">Vista previa:</Typography>
-                    <ErrorBoundary>
-                        <Activity id={content.contentId}/>
-                    </ErrorBoundary>
-                </CardContent>
-              </Card>
-              
-              }
-
-           
-    </Stack>);
+    return (
+        <Stack textAlign={"left"}>
+            <Typography variant="h6">Actividad del level: </Typography>
+            <Typography variant="body1">Descripción: {activity?.description}</Typography>
+            <Typography variant="body1">Contenido: </Typography>
+            <EditContentDialog open={openEditContent} handleClose={() => { setOpenEditContent(false) }} content={content} />
+            {isFetchingContent ? (
+                <SkeletonContent />
+            ) : isErrorContent ? (
+                <Alert severity="error">Hubo un error al obtener el contenido</Alert>
+            ) : (
+                <ContentDetails content={content} onEdit={() => setOpenEditContent(true)} />
+            )}
+        </Stack>
+    );
 }
 
 export default ActivityInfo;
